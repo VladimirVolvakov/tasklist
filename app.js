8bbb0634a051b3560037c6308f0b5ccbf3482699
VladimirVolvakov <dr.volvakov@gmail.com>
2022-06-13 11:31:59 +0300

    feat: implement add-task functionality in app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+// Get access to certain DOM elements:
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const taskFilter = document.querySelector('#task-filter');
+
+// Load all the event listeners:
+loadAllEventListeners();
+
+function loadAllEventListeners () {
+    form.addEventListener('submit', addTask);
+}
+
+// Add task function:
+function addTask (event) {
+    if (taskInput.value === '') {
+        alert('You should enter a task to add it to task list');
+    }
+
+    // Create a list-item element:
+    const listItem = document.createElement('li');
+    // Add a class to list-item element:
+    listItem.className = 'collection-item';
+    // Create a text node and append it to list-item element:
+    const textNode = document.createTextNode(taskInput.value);
+    listItem.appendChild(textNode);
+    // Create a link element:
+    const link = document.createElement('a');
+    // Add classes to link element:
+    link.className = 'delete-item secondary-content';
+    // Add delete-icon html:
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link element to list-item element:
+    listItem.appendChild(link);
+    // Append list-item element to ul.collection element:
+    taskList.appendChild(listItem);
+    // Clear input:
+    taskInput.value = '';
+
+    // Prevent a default page reload when submit the form:
+    event.preventDefault();
+}

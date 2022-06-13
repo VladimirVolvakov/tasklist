82e923337c17986ff5f15d121e58d2ad27e45475
VladimirVolvakov <dr.volvakov@gmail.com>
2022-06-13 12:19:00 +0300

    feat: implement clear-tasks button functionality in app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ loadAllEventListeners();
 function loadAllEventListeners () {
     form.addEventListener('submit', addTask);
     taskList.addEventListener('click', deleteTask);
+    clearButton.addEventListener('click', clearTasks);
 };
 
 // Add task function
@@ -45,9 +46,23 @@ function addTask (event) {
 
 // Delete task function
 function deleteTask (event) {
+    // Check if parent element of click target (X) is a link.delete-item:
     if (event.target.parentElement.classList.contains('delete-item')) {
+        // Check if user confirms task removal:
         if (confirm('Are you sure?')) {
+            // Remove list-item element that is a parent element of link.delete-item
             event.target.parentElement.parentElement.remove();
         };
     };
 };
+
+// Clear tasks function:
+function clearTasks (event) {
+    // Check if user confirms tasks removal:
+    if (confirm('Do you really want to clear ALL the tasks?')) {
+        // Get access to list-items with tasks:
+        const tasks = document.querySelectorAll('.collection-item');
+        // Start forEach loop to remove every task:
+        tasks.forEach(task => task.remove());
+    }
+}
